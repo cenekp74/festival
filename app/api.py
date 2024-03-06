31ae8b104abc1ce45a12e4ad0d38f539d4bb00f1
Čeněk Použník <potuznikcenek@gmail.com>
2024-03-06 21:07:12 +0100

    api changes

--- app/api.py
+++ app/api.py
@@ -7,33 +7,22 @@ api_blueprint = Blueprint('api', __name__)
 @api_blueprint.route('/query/film')
 def query_film():
     args = request.args.to_dict()
-    return jsonify([item.serialize for item in Film.query.filter_by(**args)])
-
-@api_blueprint.route('/query/film/all')
-def query_film_all():
-    return jsonify([item.serialize for item in Film.query.all()])
+    return jsonify([item.serialize for item in Film.query.filter_by()])
 
 @api_blueprint.route('/query/film/day')
-def query_film_by_dat_all():
+def query_film_by_day():
     result = []
     for day in [1, 2, 3]:
         result.append([item.serialize for item in Film.query.filter_by(day=day)])
     return jsonify(result)
 
-@api_blueprint.route('/query/program_items/all')
-def query_all_program_items():
+@api_blueprint.route('/query/program_items')
+def query_program_items():
+    args = request.args.to_dict()
     result = []
-    result += [item.serialize for item in Film.query.all()]
-    result += [item.serialize for item in Beseda.query.all()]
-    result += [item.serialize for item in Workshop.query.all()]
-    return jsonify(result)
-
-@api_blueprint.route('/query/program_items/day=<day>')
-def query_program_items_by_day(day):
-    result = []
-    result += [item.serialize for item in Film.query.filter_by(day=day)]
-    result += [item.serialize for item in Beseda.query.filter_by(day=day)]
-    result += [item.serialize for item in Workshop.query.filter_by(day=day)]
+    result += [item.serialize for item in Film.query.filter_by(**args)]
+    result += [item.serialize for item in Beseda.query.filter_by(**args)]
+    result += [item.serialize for item in Workshop.query.filter_by(**args)]
     return jsonify(result)
 
 @api_blueprint.route('/query/program_items/day')
