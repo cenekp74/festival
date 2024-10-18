72d8bbc6be47d543497bf81b200a1b9838216318
Čeněk Použník <potuznikcenek@gmail.com>
2024-10-18 19:45:21 +0200

    added ?forcedisplay option to wip mode

--- app/decorators.py
+++ app/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask_login import current_user
 from app import app
-from flask import redirect, url_for
+from flask import redirect, url_for, request
 
 def admin_required(func):
     @wraps(func)
@@ -14,7 +14,7 @@ def wip_disabled(func): # decorator pro funkce co maj bejt zakazany v WIP modu
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if app.config['WIP_MODE']:
-            if not current_user.is_authenticated:
+            if not current_user.is_authenticated and not "force-display" in request.args:
                 return redirect(url_for('wip'))
         return func(*args, **kwargs)
     return decorated_function
