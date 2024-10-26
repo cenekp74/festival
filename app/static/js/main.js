2e647eebf41484e077983394cdf5cd07ba371cb1
Čeněk Použník <potuznikcenek@gmail.com>
2024-10-26 19:41:50 +0200

    interactive program edit flash alert now disappears after 5s

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -96,13 +96,19 @@ function deleteAllFlashAlerts() {
 }
 
 /** prida na stranku flash alert s obsahem message a tridou alert-$category */
-function showFlashAlert(message, category='') {
+function showFlashAlert(message, category='', timeout=5000) {
     let eleString = `<div class="alert alert-${category}">
             ${message} <i onclick="setParentDisplayNone(this)" class="fa fa-xmark"></i>
         </div>`
     let ele = createElementFromHTML(eleString)
     let content = document.getElementsByClassName('content')[0]
     content.insertBefore(ele, content.firstChild)
+    setTimeout(function() { // tahle monstrozita je tu aby po chvili alert zmizel a aby mizel postupne
+        ele.style.opacity = '0'
+        setTimeout(function() {
+            ele.style.display = 'none'
+        }, timeout*1/3)
+    }, timeout*2/3)
 }
 
 function getCookie(name) {
